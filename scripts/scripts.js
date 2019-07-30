666f9329c10ce0451cb5a22adbf08eb8715a8612
Caitlin <caitlin@griffus.com>
2019-07-30 19:59:42 -0400

    styling for contact cards added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,40 +1,31 @@
-"use strict";
+const myContacts = [];
+const form = document.querySelector(".input_contacts");
 
-class AddressBook {
-  constructor() {
-    this.contacts = [];
-  }
-
-  add(name, email, phone, relation) {
-    let newContact = new Contacts(name, email, phone, relation);
-    this.contacts.push(newContact);
-  }
-
-  delete(name) {
-    const index = this.contacts.findIndex(contact => {
-      return contact.name === name;
-    });
-    if (index >= 0) {
-      this.contacts.splice(index, 1);
-    }
-  }
-
-  print() {
-    this.contacts.forEach(contact => {
-      console.log(contact);
-    });
-  }
+function handleAdd(event) {
+  event.preventDefault();
+  myContacts.push({
+    name: event.target[0].value,
+    email: event.target[1].value,
+    phone: event.target[2].value,
+    relation: event.target[3].value
+  });
+  displayContacts();
 }
 
-class Contacts {
-  constructor(name, email, phone, relation) {
-    this.name = name;
-    this.email = email;
-    this.phone = phone;
-    this.relation = relation;
-  }
+function displayContacts() {
+  document.querySelector(".contact-card").innerHTML = "";
+  form.reset();
+  myContacts.forEach((person, index) => {
+    const div = document.createElement("div");
+    div.classList.add("contact-info");
+    div.innerHTML = `
+    <p>Name: ${person.name}</p>
+    <p>Email: ${person.email}</p>
+    <p>Phone: ${person.phone}</p>
+    <p>Relation: ${person.relation}</p>
+    <button id = "delete_btn"><i class="fas fa-trash-alt"></i></button>`;
+    document.querySelector(".contact-card").append(div);
+  });
 }
 
-const myContacts = new AddressBook();
-
-myContacts.print();
+form.addEventListener("submit", handleAdd);
