90804d80a2929c053e283607b93ba4922f5363ee
Caitlin <caitlin@griffus.com>
2019-07-30 20:24:16 -0400

    added delete handle

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -23,9 +23,17 @@ function displayContacts() {
     <p>Email: ${person.email}</p>
     <p>Phone: ${person.phone}</p>
     <p>Relation: ${person.relation}</p>
-    <button id = "delete_btn"><i class="fas fa-trash-alt"></i></button>`;
+    <button index = ${index} id = "delete_btn"><i class="fas fa-trash-alt"></i></button>`;
     document.querySelector(".contact-card").append(div);
   });
 }
 
+function deleteContactHandler(event) {
+  myContacts.splice(Number(event.target.attributes[0].value), 1);
+  displayContacts();
+}
+
 form.addEventListener("submit", handleAdd);
+document
+  .querySelector(".contact-card")
+  .addEventListener("click", deleteContactHandler);
